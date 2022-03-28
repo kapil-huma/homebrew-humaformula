70f676054fe453023f1d570f7e822d098db7e391
kapil-huma <kapil@huma.ai>
2022-03-28 16:04:23 +0530

    uuuu

---
+++ humaformula.rb
@@ -0,0 +1,11 @@
+class Humaformula < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/kapil-huma/humaformula/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "b414de04753e6a800a92d810a85b0b140371fb12099e2136a6e1d3701a17bc51"
+
+  def install
+	     bin.install "humaformula"
+
+  end
+end
